965e11edc6cd5da1302364b1fc0d6e9c34c25476
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2021-02-28 23:56:12 +0600

    Delete BankService.java

--- src/main/java/com/example/demospring/BankService.java
+++
@@ -1,8 +0,0 @@
-package demospring;
-
-public interface BankService {
-    double checkBalance(String cardNumber);
-    boolean withdraw(double amount, String cardNumber);
-    boolean topUp(double amount, String cardNumber);
-    void changePinCode(String newPinCode, String cardNumber);
-}
